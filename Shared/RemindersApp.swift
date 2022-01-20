e0ca05bbd0511f400347a84c336d1c8a64a2569e
yining wang <ywang@lcs.on.ca>
2022-01-20 11:19:59 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Yining Wang on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
