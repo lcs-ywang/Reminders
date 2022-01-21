b8ca798af6f815a84275e7b747b69dd1d16d945b
yining wang <ywang@lcs.on.ca>
2022-01-20 20:23:52 -0500

    Connected Content view

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -10,14 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
    
-    @ObservedObject var store: TaskStore 
+    @ObservedObject var store: TaskStore
+    
+    @State private var showingAddTask = false
+    
     var body: some View {
         List(store.tasks){task in
             TaskCell(task: task)
           
         }
         .navigationTitle("Reminders")
-         
+        .toolbar{
+            ToolbarItem(placement: .primaryAction) {
+                Button("Add"){
+                    showingAddTask = true
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddTask){
+            AddTask(showing: $showingAddTask)
+        }
     }
 }
 
