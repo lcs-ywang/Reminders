755e267e3bad263e8e958f7b1e3a1c0085a9816e
yining wang <ywang@lcs.on.ca>
2022-01-26 21:41:54 -0500

    added a picker to let user decide which task to show based on its priority

---
+++ Shared/Model/VisibleTaskPriority.swift
@@ -0,0 +1,17 @@
+//
+//  VisibleTaskPriority.swift
+//  Reminders
+//
+//  Created by Yining Wang on 2022-01-26.
+//
+
+import Foundation
+import SwiftUI
+
+enum VisibleTaskPriority: String {
+    case high = "High"
+    case medium = "Medium"
+    case low = "Low"
+    case all = "All"
+}
+
--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -18,50 +18,72 @@ struct ContentView: View {
     
     @State var listShouldUpdate = false
     
+    @State private var selectedPriorityForVisibleTasks: VisibleTaskPriority = .all
+    
+    
     var body: some View {
         
         let _ = print("listShouldUpdate has been toggled. Current value is: \(listShouldUpdate)")
+        let _ = print("Filtering tasks by this priority: \(selectedPriorityForVisibleTasks)")
         
-        List{
-            ForEach(store.tasks){task in
-                
-                if showingCompletedTasks {
+        VStack {
+            
+            Text("Filter by...")
+                .font(Font.caption.smallCaps())
+                .foregroundColor(.secondary)
+            
+            Picker("Priority", selection: $selectedPriorityForVisibleTasks) {
+                Text(VisibleTaskPriority.all.rawValue)
+                    .tag(VisibleTaskPriority.all)
+                Text(VisibleTaskPriority.low.rawValue)
+                    .tag(VisibleTaskPriority.low)
+
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            
+            
+            List{
+                ForEach(store.tasks){task in
                     
-                    TaskCell(task: task, triggerListUpdate: .constant(true))
+                    if showingCompletedTasks {
+                        
+                        TaskCell(task: task, triggerListUpdate: .constant(true))
+                        
+                    }else{
+                        if task.completed == false {
+                            TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                        }
+                    }
                     
-                }else{
-                    if task.completed == false {
-                        TaskCell(task: task, triggerListUpdate: $listShouldUpdate)
+                       
+                  
+                }
+                .onDelete(perform: store.deleteItems)
+                .onMove(perform: store.moveItems)
+            }
+            .navigationTitle("Reminders")
+            .toolbar{
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Add"){
+                        showingAddTask = true
                     }
                 }
-                
-                   
-              
-            }
-            .onDelete(perform: store.deleteItems)
-            .onMove(perform: store.moveItems)
-        }
-        .navigationTitle("Reminders")
-        .toolbar{
-            ToolbarItem(placement: .primaryAction) {
-                Button("Add"){
-                    showingAddTask = true
+                ToolbarItem(placement: .navigationBarLeading){
+                    EditButton()
+                }
+                ToolbarItem(placement: .bottomBar){
+                    //    Condition to evaluate     When True                  When false
+                    Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
+                        print("Value of showingCompletedTasks was: \(showingCompletedTasks )")
+                        showingCompletedTasks.toggle()
+                        print("Value of showingCompletedTasks is now: \(showingCompletedTasks )")
+                    }
                 }
             }
-            ToolbarItem(placement: .navigationBarLeading){
-                EditButton()
-            }
-            ToolbarItem(placement: .bottomBar){
-                //    Condition to evaluate     When True                  When false
-                Button(showingCompletedTasks ? "Hide Completed Tasks" : "Show Completed Tasks") {
-                    print("Value of showingCompletedTasks was: \(showingCompletedTasks )")
-                    showingCompletedTasks.toggle()
-                    print("Value of showingCompletedTasks is now: \(showingCompletedTasks )")
-                }
-            }
+            .sheet(isPresented: $showingAddTask){
+                AddTask(store: store, showing: $showingAddTask)
         }
-        .sheet(isPresented: $showingAddTask){
-            AddTask(store: store, showing: $showingAddTask)
         }
     }
 }
